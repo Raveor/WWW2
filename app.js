d95cf277ea096124e4120f135c2de2e879cf374f
Maciek Książkiewicz <ksiazkiewiczmaciej@outlook.com>
2019-04-08 17:29:06 +0200

    facebook login

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var authRouter = require('./routes/auth')
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(session({ secret: 'anything' }));
+app.use(passport.initialize());
+app.use(passport.session({ secret: 'secret', saveUninitialized: false,
+resave: false, cookie: { maxAge: 1000 } }));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(req.user);
+  if(req.user == undefined) {
+    res.render('index', { title: 'Express' });
+  } else {
+    res.render('index', { title: 'Express', user: req.user.name });
+  }
 });
 
 module.exports = router;
